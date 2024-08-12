5950e4245e3b9b34b809cc5c1b16883be1308f70
claude89757 <claudexie@outlook.com>
2024-08-13 00:34:13 +0800

    fix

--- pages/2_Filter_Data.py
+++ pages/2_Filter_Data.py
@@ -67,35 +67,30 @@ src_dir = f"./data/{st.session_state.access_code}/raw/"
 dst_dir = f"./data/{st.session_state.access_code}/processed/"
 
 files = [f for f in os.listdir(src_dir) if os.path.isfile(os.path.join(src_dir, f))]
-selected_file = st.selectbox("Select a file to analyze:", files)
-selected_file_path = None
-if selected_file:
-    selected_file_path = os.path.join(src_dir, selected_file)
-    st.subheader(f"File Data Preview: {selected_file}")
-    data = pd.read_csv(selected_file_path)
-    st.write(data)
+if files:
+    st.session_state.selected_file = st.selectbox("Select a file to analyze:", files)
+    selected_file_path = os.path.join(src_dir, st.session_state.selected_file)
+    st.subheader(f"File Data Preview: {st.session_state.selected_file}")
+    # 选择确定处理的文件
+    if st.session_state.selected_file:
+        st.query_params.selected_file = st.session_state.selected_file
+        local_file_path = os.path.join(src_dir, st.session_state.selected_file)
+        # 检查本地是否已有文件
+        try:
+            data = pd.read_csv(local_file_path)
+            # 展示数据
+            if data is not None:
+                st.dataframe(data)
+            else:
+                st.write("No data to display.")
+        except Exception as e:
+            st.error(f"Error loading data from local file: {e}")
+    else:
+        st.error("No selected file.")
 else:
     st.warning("No processed data, return to filter data...")
     time.sleep(3)
     st.switch_page("pages/2_Filter_Data.py")
-
-
-# 选择确定处理的文件
-if selected_file:
-    st.query_params.selected_file = selected_file
-    local_file_path = os.path.join(src_dir, selected_file)
-    # 检查本地是否已有文件
-    try:
-        data = pd.read_csv(local_file_path)
-        # 展示数据
-        if data is not None:
-            st.dataframe(data)
-        else:
-            st.write("No data to display.")
-    except Exception as e:
-        st.error(f"Error loading data from local file: {e}")
-else:
-    st.error("No selected file.")
 
 
 col1, col2 = st.columns(2)
@@ -103,8 +98,8 @@ col1, col2 = st.columns(2)
 with col1:
     # Button to confirm the file
     if st.button("Confirm File ", type="primary"):
-        src_file_path = os.path.join(src_dir, selected_file)
-        dst_file_path = os.path.join(dst_dir, selected_file)
+        src_file_path = os.path.join(src_dir, st.session_state.selected_file)
+        dst_file_path = os.path.join(dst_dir, st.session_state.selected_file)
         shutil.move(src_file_path, dst_file_path)
         st.success(f"Confirmed date successfully, entering step...")
         time.sleep(3)
