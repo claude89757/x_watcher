38ac0165ca0608067c2b75acb87647521f007246
claude89757 <claudexie@outlook.com>
2024-08-12 22:46:55 +0800

    fix

--- collectors/x_collector_api.py
+++ collectors/x_collector_api.py
@@ -14,21 +14,7 @@ from quart import Quart
 from quart import request
 
 from config import CONFIG
-
-
-from pathlib import Path
-
-# 获取当前工作目录
-current_path = Path.cwd()
-print("当前工作目录:", current_path)
-
-# 获取当前脚本的路径
-script_path = Path(__file__).resolve()
-print("当前脚本路径:", script_path)
-
-# 获取当前脚本所在的目录
-script_dir = script_path.parent
-print("当前脚本所在目录:", script_dir)
+from x_collect import TwitterWatcher
 
 # 配置日志
 logging.basicConfig(level=logging.INFO)
@@ -36,10 +22,21 @@ logging.basicConfig(level=logging.INFO)
 app = Quart(__name__)
 
 
-async def collect_data_from_x(username, email, password, search_key_word, max_post_num, access_code):
-    # 模拟异步数据收集
-    await asyncio.sleep(1)
-    # 这里写你的实际数据收集逻辑
+async def async_collect_data_from_x(username, email, password, search_key_word, max_post_num, access_code):
+    """
+    异步收集数据
+    :param username:
+    :param email:
+    :param password:
+    :param search_key_word:
+    :param max_post_num:
+    :param access_code:
+    :return:
+    """
+    logging.info("start collecting data.")
+    watcher = TwitterWatcher('/usr/local/bin/chromedriver', username, email, password, search_key_word)
+    watcher.run(max_post_num, access_code)
+    logging.info("done collecting data.")
     return "Collected data"
 
 
@@ -63,9 +60,9 @@ async def webhook():
                 password = collector_username_infos[username]['password']
 
                 # 异步调用数据收集函数
-                await collect_data_from_x(username=username, email=email, password=password,
-                                          search_key_word=search_key_word, max_post_num=max_post_num,
-                                          access_code=access_code)
+                await async_collect_data_from_x(username=username, email=email, password=password,
+                                                search_key_word=search_key_word, max_post_num=max_post_num,
+                                                access_code=access_code)
                 return 'Success', 200
             else:
                 return 'Missing username\'s info', 500
