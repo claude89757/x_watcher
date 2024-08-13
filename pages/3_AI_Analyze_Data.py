9a59fd0036681c20a947b381e68abea2676bf79d
claude89757 <claudexie@outlook.com>
2024-08-13 13:15:43 +0800

    fix

--- pages/3_AI_Analyze_Data.py
+++ pages/3_AI_Analyze_Data.py
@@ -113,21 +113,30 @@ logging.basicConfig(format="\n%(asctime)s\n%(message)s", level=logging.INFO, for
 
 st.set_page_config(page_title="Analyze Data", page_icon="🤖", layout="wide")
 
-if st.session_state.get('access_code') and st.session_state.get('access_code') in CONFIG['access_code_list']:
-    st.query_params.access_code = st.session_state.access_code
-elif st.query_params.get('access_code') and st.query_params.get('access_code') in CONFIG['access_code_list']:
-    st.session_state.access_code = st.query_params.access_code
-else:
-    st.warning("Access not Granted!")
-    st.switch_page("Home.py", )
+# if st.session_state.get('access_code') and st.session_state.get('access_code') in CONFIG['access_code_list']:
+#     st.query_params.access_code = st.session_state.access_code
+# elif st.query_params.get('access_code') and st.query_params.get('access_code') in CONFIG['access_code_list']:
+#     st.session_state.access_code = st.query_params.access_code
+# else:
+#     st.warning("Access not Granted!")
+#     st.switch_page("Home.py", )
 
 # 初始化session state
+if 'access_code' not in st.session_state:
+    st.session_state.access_code = st.query_params.get('access_code')
 if "search_keyword" not in st.session_state:
     st.session_state.search_keyword = st.query_params.get("search_keyword")
 if "matching_files" not in st.session_state:
     st.session_state.matching_files = ""
 if "analysis_run" not in st.session_state:
     st.session_state.analysis_run = False
+
+if st.session_state.access_code:
+    st.query_params.access_code = st.session_state.access_code
+else:
+    st.warning("Access not Granted!")
+    time.sleep(3)
+    st.switch_page("Home.py", )
 
 # 强制响应式布局
 st.write(
